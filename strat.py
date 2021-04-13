b807a4360b3d535c1925a86a98ace32cee487021
mcranium <42519751+mcranium@users.noreply.github.com>
2021-04-13 17:07:09 +0200

    Add files via upload

---
+++ strat.py
@@ -0,0 +1,224 @@
+from sty import fg, bg, rs
+
+age001 = fg(0, 0, 0) + bg(255, 244, 240) + ' Quaternary    | Holocene      | Meghalayan    | 0.004  Ma to 0      Ma ' + rs.all
+age002 = fg(0, 0, 0) + bg(254, 246, 243) + ' Quaternary    | Holocene      | Northgrippian | 0.008  Ma to 0.004  Ma ' + rs.all
+age003 = fg(0, 0, 0) + bg(253, 245, 242) + ' Quaternary    | Holocene      | Greenlandian  | 0.011  Ma to 0.008  Ma ' + rs.all
+age004 = fg(0, 0, 0) + bg(255, 242, 221) + ' Quaternary    | Pleistocene   | Upper         | 0.126  Ma to 0.011  Ma ' + rs.all
+age005 = fg(0, 0, 0) + bg(255, 241, 211) + ' Quaternary    | Pleistocene   | Middle        | 0.781  Ma to 0.126  Ma ' + rs.all
+age006 = fg(0, 0, 0) + bg(255, 240, 202) + ' Quaternary    | Pleistocene   | Calabrian     | 1.8    Ma to 0.781  Ma ' + rs.all
+age007 = fg(0, 0, 0) + bg(255, 238, 183) + ' Quaternary    | Pleistocene   | Gelasian      | 2.58   Ma to 1.8    Ma ' + rs.all
+age008 = fg(0, 0, 0) + bg(255, 249, 207) + ' Neogene       | Pliocene      | Piacenzian    | 3.6    Ma to 2.58   Ma ' + rs.all
+age009 = fg(0, 0, 0) + bg(255, 248, 197) + ' Neogene       | Pliocene      | Zanclean      | 5.333  Ma to 3.6    Ma ' + rs.all
+age010 = fg(0, 0, 0) + bg(255, 243, 149) + ' Neogene       | Miocene       | Messinian     | 7.246  Ma to 5.333  Ma ' + rs.all
+age011 = fg(0, 0, 0) + bg(255, 242, 139) + ' Neogene       | Miocene       | Tortonian     | 11.63  Ma to 7.246  Ma ' + rs.all
+age012 = fg(0, 0, 0) + bg(255, 241, 129) + ' Neogene       | Miocene       | Serravallian  | 13.82  Ma to 11.63  Ma ' + rs.all
+age013 = fg(0, 0, 0) + bg(255, 240, 120) + ' Neogene       | Miocene       | Langhian      | 15.97  Ma to 13.82  Ma ' + rs.all
+age014 = fg(0, 0, 0) + bg(255, 239, 110) + ' Neogene       | Miocene       | Burdigalian   | 20.44  Ma to 15.97  Ma ' + rs.all
+age015 = fg(0, 0, 0) + bg(255, 238, 100) + ' Neogene       | Miocene       | Aquitanian    | 23.03  Ma to 20.44  Ma ' + rs.all
+age016 = fg(0, 0, 0) + bg(255, 229, 188) + ' Palaeogene    | Oligocene     | Chattian      | 27.82  Ma to 23.03  Ma ' + rs.all
+age017 = fg(0, 0, 0) + bg(255, 219, 174) + ' Palaeogene    | Oligocene     | Rupelian      | 33.9   Ma to 27.82  Ma ' + rs.all
+age018 = fg(0, 0, 0) + bg(255, 210, 179) + ' Palaeogene    | Eocene        | Priabonian    | 37.8   Ma to 33.9   Ma ' + rs.all
+age019 = fg(0, 0, 0) + bg(255, 200, 165) + ' Palaeogene    | Eocene        | Bartonian     | 41.2   Ma to 37.8   Ma ' + rs.all
+age020 = fg(0, 0, 0) + bg(255, 189, 152) + ' Palaeogene    | Eocene        | Lutetian      | 47.8   Ma to 41.2   Ma ' + rs.all
+age021 = fg(0, 0, 0) + bg(255, 179, 140) + ' Palaeogene    | Eocene        | Ypresian      | 56     Ma to 47.8   Ma ' + rs.all
+age022 = fg(0, 0, 0) + bg(255, 197, 139) + ' Palaeogene    | Palaeocene    | Thanetian     | 59.2   Ma to 56     Ma ' + rs.all
+age023 = fg(0, 0, 0) + bg(255, 196, 130) + ' Palaeogene    | Palaeocene    | Selandian     | 61.6   Ma to 59.2   Ma ' + rs.all
+age024 = fg(0, 0, 0) + bg(255, 187, 126) + ' Palaeogene    | Palaeocene    | Danian        | 66     Ma to 61.6   Ma ' + rs.all
+age025 = fg(0, 0, 0) + bg(249, 241, 168) + ' Cretaceous    | Upper         | Maastrichtian | 72.1   Ma to 66     Ma ' + rs.all
+age026 = fg(0, 0, 0) + bg(235, 236, 158) + ' Cretaceous    | Upper         | Campanian     | 83.6   Ma to 72.1   Ma ' + rs.all
+age027 = fg(0, 0, 0) + bg(223, 232, 149) + ' Cretaceous    | Upper         | Santonian     | 86.3   Ma to 83.6   Ma ' + rs.all
+age028 = fg(0, 0, 0) + bg(210, 227, 140) + ' Cretaceous    | Upper         | Coniacian     | 89.8   Ma to 86.3   Ma ' + rs.all
+age029 = fg(0, 0, 0) + bg(197, 223, 131) + ' Cretaceous    | Upper         | Turonian      | 93.9   Ma to 89.8   Ma ' + rs.all
+age030 = fg(0, 0, 0) + bg(184, 218, 122) + ' Cretaceous    | Upper         | Cenomanian    | 100.5  Ma to 93.9   Ma ' + rs.all
+age031 = fg(0, 0, 0) + bg(206, 231, 177) + ' Cretaceous    | Lower         | Albian        | 113    Ma to 100.5  Ma ' + rs.all
+age032 = fg(0, 0, 0) + bg(193, 226, 168) + ' Cretaceous    | Lower         | Aptian        | 125    Ma to 113    Ma ' + rs.all
+age033 = fg(0, 0, 0) + bg(180, 222, 159) + ' Cretaceous    | Lower         | Barremian     | 129.4  Ma to 125    Ma ' + rs.all
+age034 = fg(0, 0, 0) + bg(167, 217, 150) + ' Cretaceous    | Lower         | Hauterivian   | 132.9  Ma to 129.4  Ma ' + rs.all
+age035 = fg(0, 0, 0) + bg(154, 213, 141) + ' Cretaceous    | Lower         | Valanginian   | 139.8  Ma to 132.9  Ma ' + rs.all
+age036 = fg(0, 0, 0) + bg(142, 209, 132) + ' Cretaceous    | Lower         | Berriasian    | 145    Ma to 139.8  Ma ' + rs.all
+age037 = fg(0, 0, 0) + bg(212, 243, 253) + ' Jurassic      | Upper         | Tithonian     | 152.1  Ma to 145    Ma ' + rs.all
+age038 = fg(0, 0, 0) + bg(198, 239, 252) + ' Jurassic      | Upper         | Kimmeridgian  | 157.3  Ma to 152.1  Ma ' + rs.all
+age039 = fg(0, 0, 0) + bg(183, 236, 252) + ' Jurassic      | Upper         | Oxfordian     | 163.5  Ma to 157.3  Ma ' + rs.all
+age040 = fg(0, 0, 0) + bg(185, 235, 242) + ' Jurassic      | Middle        | Callovian     | 166.1  Ma to 163.5  Ma ' + rs.all
+age041 = fg(0, 0, 0) + bg(170, 231, 242) + ' Jurassic      | Middle        | Bathonian     | 168.3  Ma to 166.1  Ma ' + rs.all
+age042 = fg(0, 0, 0) + bg(156, 227, 241) + ' Jurassic      | Middle        | Bajocian      | 170.3  Ma to 168.3  Ma ' + rs.all
+age043 = fg(0, 0, 0) + bg(141, 224, 240) + ' Jurassic      | Middle        | Aalenian      | 174.1  Ma to 170.3  Ma ' + rs.all
+age044 = fg(0, 0, 0) + bg(141, 216, 244) + ' Jurassic      | Lower         | Toarcian      | 182.7  Ma to 174.1  Ma ' + rs.all
+age045 = fg(0, 0, 0) + bg(112, 209, 243) + ' Jurassic      | Lower         | Pliensbachian | 190.8  Ma to 182.7  Ma ' + rs.all
+age046 = fg(0, 0, 0) + bg(84, 203, 241)  + ' Jurassic      | Lower         | Sinemurian    | 199.3  Ma to 190.8  Ma ' + rs.all
+age047 = fg(0, 0, 0) + bg(55, 196, 240)  + ' Jurassic      | Lower         | Hettangian    | 201.3  Ma to 199.3  Ma ' + rs.all
+age048 = fg(0, 0, 0) + bg(227, 200, 226) + ' Triassic      | Upper         | Rhaetian      | 208.5  Ma to 201.3  Ma ' + rs.all
+age049 = fg(0, 0, 0) + bg(213, 187, 221) + ' Triassic      | Upper         | Norian        | 227    Ma to 208.5  Ma ' + rs.all
+age050 = fg(0, 0, 0) + bg(200, 175, 215) + ' Triassic      | Upper         | Carnian       | 237    Ma to 227    Ma ' + rs.all
+age051 = fg(0, 0, 0) + bg(201, 156, 205) + ' Triassic      | Middle        | Ladinian      | 242    Ma to 237    Ma ' + rs.all
+age052 = fg(0, 0, 0) + bg(188, 144, 199) + ' Triassic      | Middle        | Anisian       | 247.2  Ma to 242    Ma ' + rs.all
+age053 = fg(0, 0, 0) + bg(177, 113, 184) + ' Triassic      | Lower         | Olenekian     | 251.2  Ma to 247.2  Ma ' + rs.all
+age054 = fg(0, 0, 0) + bg(165, 102, 179) + ' Triassic      | Lower         | Induan        | 251.9  Ma to 251.2  Ma ' + rs.all
+age055 = fg(0, 0, 0) + bg(255, 202, 192) + ' Permian       | Lopingian     | Changhsingian | 254.14 Ma to 251.9  Ma ' + rs.all
+age056 = fg(0, 0, 0) + bg(255, 192, 179) + ' Permian       | Lopingian     | Wuchiapingian | 259.1  Ma to 254.14 Ma ' + rs.all
+age057 = fg(0, 0, 0) + bg(255, 170, 153) + ' Permian       | Guadalupian   | Capitanian    | 265.1  Ma to 259.1  Ma ' + rs.all
+age058 = fg(0, 0, 0) + bg(255, 160, 141) + ' Permian       | Guadalupian   | Wordian       | 268.8  Ma to 265.1  Ma ' + rs.all
+age059 = fg(0, 0, 0) + bg(255, 149, 129) + ' Permian       | Guadalupian   | Roadian       | 272.95 Ma to 268.8  Ma ' + rs.all
+age060 = fg(0, 0, 0) + bg(233, 155, 141) + ' Permian       | Cisuralian    | Kungurian     | 283.5  Ma to 272.95 Ma ' + rs.all
+age061 = fg(0, 0, 0) + bg(234, 145, 129) + ' Permian       | Cisuralian    | Artinskian    | 290.1  Ma to 283.5  Ma ' + rs.all
+age062 = fg(0, 0, 0) + bg(234, 135, 118) + ' Permian       | Cisuralian    | Sakmarian     | 295    Ma to 290.1  Ma ' + rs.all
+age063 = fg(0, 0, 0) + bg(235, 125, 107) + ' Permian       | Cisuralian    | Asselian      | 298.9  Ma to 295    Ma ' + rs.all
+age064 = fg(0, 0, 0) + bg(201, 218, 214) + ' Carboniferous | Pennsylvanian | Gzhelian      | 303.7  Ma to 298.9  Ma ' + rs.all
+age065 = fg(0, 0, 0) + bg(187, 215, 214) + ' Carboniferous | Pennsylvanian | Kasimovian    | 307    Ma to 303.7  Ma ' + rs.all
+age066 = fg(0, 0, 0) + bg(175, 211, 204) + ' Carboniferous | Pennsylvanian | Moscovian     | 315.2  Ma to 307    Ma ' + rs.all
+age067 = fg(0, 0, 0) + bg(147, 204, 204) + ' Carboniferous | Pennsylvanian | Bashkirian    | 323.2  Ma to 315.2  Ma ' + rs.all
+age068 = fg(0, 0, 0) + bg(196, 199, 138) + ' Carboniferous | Mississippian | Serpukhovian  | 330.9  Ma to 323.2  Ma ' + rs.all
+age069 = fg(0, 0, 0) + bg(169, 193, 139) + ' Carboniferous | Mississippian | Visean        | 346.7  Ma to 330.9  Ma ' + rs.all
+age070 = fg(0, 0, 0) + bg(142, 187, 139) + ' Carboniferous | Mississippian | Tournaisian   | 358.9  Ma to 346.7  Ma ' + rs.all
+age071 = fg(0, 0, 0) + bg(244, 237, 211) + ' Devonian      | Upper         | Famennian     | 372.2  Ma to 358.9  Ma ' + rs.all
+age072 = fg(0, 0, 0) + bg(246, 235, 192) + ' Devonian      | Upper         | Frasnian      | 382.7  Ma to 372.2  Ma ' + rs.all
+age073 = fg(0, 0, 0) + bg(248, 223, 160) + ' Devonian      | Middle        | Givetian      | 387.7  Ma to 382.7  Ma ' + rs.all
+age074 = fg(0, 0, 0) + bg(249, 213, 146) + ' Devonian      | Middle        | Eifelian      | 393.3  Ma to 387.7  Ma ' + rs.all
+age075 = fg(0, 0, 0) + bg(235, 209, 147) + ' Devonian      | Lower         | Emsian        | 407.6  Ma to 393.3  Ma ' + rs.all
+age076 = fg(0, 0, 0) + bg(236, 200, 134) + ' Devonian      | Lower         | Pragian       | 410.8  Ma to 407.6  Ma ' + rs.all
+age077 = fg(0, 0, 0) + bg(236, 190, 122) + ' Devonian      | Lower         | Lochkovian    | 419.2  Ma to 410.8  Ma ' + rs.all
+age078 = fg(0, 0, 0) + bg(228, 245, 235) + ' Silurian      | Pridoli       |               | 423    Ma to 419.2  Ma ' + rs.all
+age079 = fg(0, 0, 0) + bg(214, 241, 234) + ' Silurian      | Ludlow        | Ludfordian    | 425.6  Ma to 423    Ma ' + rs.all
+age080 = fg(0, 0, 0) + bg(200, 237, 233) + ' Silurian      | Ludlow        | Gorstian      | 427.4  Ma to 425.6  Ma ' + rs.all
+age081 = fg(0, 0, 0) + bg(201, 236, 224) + ' Silurian      | Wenlock       | Homerian      | 430.5  Ma to 427.4  Ma ' + rs.all
+age082 = fg(0, 0, 0) + bg(188, 231, 214) + ' Silurian      | Wenlock       | Sheinwoodian  | 433.4  Ma to 430.5  Ma ' + rs.all
+age083 = fg(0, 0, 0) + bg(187, 232, 224) + ' Silurian      | Llandovery    | Telychian     | 438.5  Ma to 433.4  Ma ' + rs.all
+age084 = fg(0, 0, 0) + bg(174, 228, 214) + ' Silurian      | Llandovery    | Aeronian      | 440.8  Ma to 438.5  Ma ' + rs.all
+age085 = fg(0, 0, 0) + bg(160, 223, 204) + ' Silurian      | Llandovery    | Rhuddanian    | 443.8  Ma to 440.8  Ma ' + rs.all
+age086 = fg(0, 0, 0) + bg(162, 222, 195) + ' Ordovician    | Upper         | Hirnantian    | 445.2  Ma to 443.8  Ma ' + rs.all
+age087 = fg(0, 0, 0) + bg(147, 218, 195) + ' Ordovician    | Upper         | Katian        | 453    Ma to 445.2  Ma ' + rs.all
+age088 = fg(0, 0, 0) + bg(136, 213, 177) + ' Ordovician    | Upper         | Sandbian      | 458.4  Ma to 453    Ma ' + rs.all
+age089 = fg(0, 0, 0) + bg(106, 207, 186) + ' Ordovician    | Middle        | Darriwilian   | 467.3  Ma to 458.4  Ma ' + rs.all
+age090 = fg(0, 0, 0) + bg(93, 203, 177)  + ' Ordovician    | Middle        | Dapingian     | 470    Ma to 467.3  Ma ' + rs.all
+age091 = fg(0, 0, 0) + bg(52, 193, 168)  + ' Ordovician    | Lower         | Floian        | 477.7  Ma to 470    Ma ' + rs.all
+age092 = fg(0, 0, 0) + bg(40, 189, 160)  + ' Ordovician    | Lower         | Tremadocian   | 485.4  Ma to 477.7  Ma ' + rs.all
+age093 = fg(0, 0, 0) + bg(230, 243, 215) + ' Cambrian      | Furongian     | Stage 10      | 489.5  Ma to 485.4  Ma ' + rs.all
+age094 = fg(0, 0, 0) + bg(217, 238, 206) + ' Cambrian      | Furongian     | Jiangshanian  | 494    Ma to 489.5  Ma ' + rs.all
+age095 = fg(0, 0, 0) + bg(204, 233, 196) + ' Cambrian      | Furongian     | Paibian       | 497    Ma to 494    Ma ' + rs.all
+age096 = fg(0, 0, 0) + bg(204, 224, 191) + ' Cambrian      | Miaolingian   | Guzhangian    | 500.5  Ma to 497    Ma ' + rs.all
+age097 = fg(0, 0, 0) + bg(191, 220, 182) + ' Cambrian      | Miaolingian   | Drumian       | 504.5  Ma to 500.5  Ma ' + rs.all
+age098 = fg(0, 0, 0) + bg(178, 215, 173) + ' Cambrian      | Miaolingian   | Wuliuan       | 509    Ma to 504.5  Ma ' + rs.all
+age099 = fg(0, 0, 0) + bg(179, 207, 169) + ' Cambrian      | Series 2      | Stage 4       | 514    Ma to 509    Ma ' + rs.all
+age100 = fg(0, 0, 0) + bg(166, 203, 160) + ' Cambrian      | Series 2      | Stage 3       | 521    Ma to 514    Ma ' + rs.all
+age101 = fg(0, 0, 0) + bg(167, 194, 156) + ' Cambrian      | Terreneuvian  | Stage 2       | 529    Ma to 521    Ma ' + rs.all
+age102 = fg(0, 0, 0) + bg(154, 191, 147) + ' Cambrian      | Terreneuvian  | Fortunian     | 541    Ma to 529    Ma ' + rs.all
+
+ages = (age001, 
+	age002, 
+	age003, 
+	age004, 
+	age005, 
+	age006, 
+	age007, 
+	age008, 
+	age009, 
+	age010, 
+	age011, 
+	age012, 
+	age013, 
+	age014, 
+	age015, 
+	age016, 
+	age017, 
+	age018, 
+	age019, 
+	age020, 
+	age021, 
+	age022, 
+	age023, 
+	age024, 
+	age025, 
+	age026, 
+	age027, 
+	age028, 
+	age029, 
+	age030, 
+	age031, 
+	age032, 
+	age033, 
+	age034, 
+	age035, 
+	age036, 
+	age037, 
+	age038, 
+	age039, 
+	age040, 
+	age041, 
+	age042, 
+	age043, 
+	age044, 
+	age045, 
+	age046, 
+	age047, 
+	age048, 
+	age049, 
+	age050, 
+	age051, 
+	age052, 
+	age053, 
+	age054, 
+	age055, 
+	age056, 
+	age057, 
+	age058, 
+	age059, 
+	age060, 
+	age061, 
+	age062, 
+	age063, 
+	age064, 
+	age065, 
+	age066, 
+	age067, 
+	age068, 
+	age069, 
+	age070, 
+	age071, 
+	age072, 
+	age073, 
+	age074, 
+	age075, 
+	age076, 
+	age077, 
+	age078, 
+	age079, 
+	age080, 
+	age081, 
+	age082, 
+	age083, 
+	age084, 
+	age085, 
+	age086, 
+	age087, 
+	age088, 
+	age089, 
+	age090, 
+	age091, 
+	age092, 
+	age093, 
+	age094, 
+	age095, 
+	age096, 
+	age097, 
+	age098, 
+	age099, 
+	age100, 
+	age101, 
+	age102)
+
+for i in range(102):
+	print(ages[i])
+
+print("The numerical ages are according to Ogg et al. (2016): \nA concise geologic time scale 2016, Elsevier, 234 p.")
+
+# while True:
+    # exitkey = input("Enter q to exit.")
+    # if exitkey == "q":
+        # break
+
+
+
+
+
+
+
---
+++ stratpy.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python /home/mario/Programs/StratPy/strat.py
